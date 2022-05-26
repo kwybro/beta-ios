f082db3998c6b1622606c811069d64dc6cbfcf72
kwybro <kwybro@gmail.com>
2022-05-26 15:59:06 -0400

    Create WeatherWidgetsListView

---
+++ weather/weather/Features/WeatherWidgets/WeatherWidgetsListView.swift
@@ -0,0 +1,28 @@
+//
+//  WeatherWidgetsListView.swift
+//  weather
+//
+//  Created by Kyle Wybranowski on 5/26/22.
+//
+
+import SwiftUI
+
+struct WeatherWidgetsListView<ViewModel: WeatherViewModelProtocol>: View {
+    @ObservedObject var viewModel: ViewModel
+
+    let columns: [GridItem] = [GridItem(.adaptive(minimum: 300))]
+
+    var body: some View {
+        switch viewModel.viewState {
+        case .loading: ProgressView()
+        default:
+            LazyVGrid(columns: columns, spacing: 20) {
+                ForEach(viewModel.currentWeatherWidgets, id: \.self) { data in
+                    WeatherWidgetView(data: data)
+                }
+            }
+            .padding([.top, .bottom])
+        }
+
+    }
+}
