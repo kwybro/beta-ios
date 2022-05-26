bf3defcb9dc8dc0320d67538164268f1c67af847
kwybro <kwybro@gmail.com>
2022-05-26 15:07:02 -0400

    Separate WeatherUnitView and WeatherWidget

---
+++ weather/weather/Features/CurrentWeather/WeatherUnitView.swift
@@ -0,0 +1,33 @@
+//
+//  WeatherUnitView.swift
+//  weather
+//
+//  Created by Kyle Wybranowski on 5/26/22.
+//
+
+import SwiftUI
+
+struct WeatherUnitView: View {
+
+    @State var unit: WeatherUnit
+
+    var body: some View {
+        VStack(alignment: .center) {
+            Text(Constants.temperatureInDegrees(unit.temperature))
+                .font(Font.footnote)
+                .foregroundColor(.black)
+            VStack {
+                Spacer()
+                Image(systemName: unit.type.imageName)
+                    .font(Font.system(.title))
+                    .frame(width: 30, height: 30, alignment: .center)
+                Spacer()
+            }
+            Text(unit.time)
+                .font(Font.caption2)
+                .foregroundColor(.black)
+        }
+        .frame(width: 75, height: 75)
+        .background(Color.white)
+    }
+}
